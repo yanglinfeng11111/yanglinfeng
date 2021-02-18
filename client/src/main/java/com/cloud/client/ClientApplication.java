b593c20e58a7e716215be418dae72e9fec611373
yanglf <yanglf@wsights.com>
2021-02-18 11:24:52 +0800

    修改配置文件

--- client/src/main/java/com/cloud/client/ClientApplication.java
+++ client/src/main/java/com/cloud/client/ClientApplication.java
@@ -2,11 +2,11 @@ package com.cloud.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 @EnableFeignClients
 public class ClientApplication {
 
--- provider/src/test/java/com/example/provider/ProviderApplicationTests.java
+++ provider/src/test/java/com/example/provider/ProviderApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.provider;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +10,4 @@ class ProviderApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }

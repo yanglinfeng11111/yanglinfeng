fb46950e1df2350d88cb2788e8c6c71b258e7c2a
yanglf <yanglf@wsights.com>
2021-02-18 17:34:35 +0800

    修改配置文件

--- client/src/main/java/com/cloud/client/ClientApplication.java
+++ client/src/main/java/com/cloud/client/ClientApplication.java
@@ -3,11 +3,9 @@ package com.cloud.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableEurekaServer
-@EnableFeignClients
+@SpringBootApplication
 public class ClientApplication {
 
     public static void main(String[] args) {
---
+++ discovery/src/main/java/com/cloud/discovery/controller/ConfigTestController.java
@@ -0,0 +1,19 @@
+package com.cloud.discovery.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("config")
+public class ConfigTestController {
+
+    @Value("${name}")
+    private String name;
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return name;
+    }
+}

dab4ec1ede1adac89077f52791f6f7895bba7ed4
yanglf <yanglf@wsights.com>
2021-02-20 13:28:06 +0800

    修改配置文件

--- discovery/src/main/java/com/cloud/discovery/controller/ConfigTestController.java
+++ discovery/src/main/java/com/cloud/discovery/controller/ConfigTestController.java
@@ -1,12 +1,11 @@
 package com.cloud.discovery.controller;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("config")
+@RequestMapping("/config")
 public class ConfigTestController {
 
     @Value("${name}")
